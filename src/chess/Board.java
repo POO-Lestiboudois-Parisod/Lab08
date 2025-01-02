949e73266187df70c615e54f3c463cccca64cbb6
MaximeLesti <maxime.lestiboudois@heig-vd.ch>
2025-01-02 19:13:45 +0100

    square and board

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,18 +1,59 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.List;
 import chess.Square;
+import java.util.Collection;
+import chess.PlayerColor;
+import chess.PieceType;
 
 public class Board {
     private final int NB_SQUARE_BY_LINE = 8;
-    private List<List<Square>> board;
-
-
+    private final Square[][] board = new Square[NB_SQUARE_BY_LINE][NB_SQUARE_BY_LINE];
     public Board() {
 
         for(int i = 0; i < NB_SQUARE_BY_LINE; ++i){
+            for(int j = 0; j < NB_SQUARE_BY_LINE; ++j) {
+                switch (i) {
+                    case 0: {
+                        Pawn pawn = new Pawn(PlayerColor.WHITE,PieceType.PAWN,board[i][j]);
+                    }
+                    case 1: {
+
+
+                    }
+                    case 6: {
+                        Pawn pawn = new Pawn(PlayerColor.BLACK,PieceType.PAWN,board[i][j]);
+
+                    }
+                    case 7: {
+                    }
+                    default: {
+                        board[i][j] = new Square(i, j);
+                    }
+                }
+            }
+
+            }
+
+    }
+
+    public List<Square> getAllPieces(){
+        List<Square> pieces = new ArrayList<>();
+        for(int i = 0; i < NB_SQUARE_BY_LINE; ++i){
             for(int j = 0; j < NB_SQUARE_BY_LINE; ++j){
-                board.get(i).set(j,new Square(i, j));
+                if(board[i][j].isOccupied()){
+                    pieces.add(new Square(i, j));
+                }
+            }
+        }
+        return pieces;
+    }
+
+    public void reset(){
+        for(int i = 0; i < NB_SQUARE_BY_LINE; ++i){
+            for(int j = 0; j < NB_SQUARE_BY_LINE; ++j){
+                new
             }
         }
     }
--- src/chess/Square.java
+++ src/chess/Square.java
@@ -3,13 +3,25 @@ package chess;
 public class Square {
     private final int x;
     private final int y;
+    private Piece piece;
 
     public Square(int x, int y) {
         this.x = x;
         this.y = y;
+        piece = null;
     }
+
+    public Square(int x, int y, Piece piece) {
+        this(x, y);
+        this.piece = piece;
+    }
+
     Square getPosition(){
         return new Square(x, y);
+    }
+
+    public boolean isOccupied(){
+        return false; //TODO
     }
 
     @Override
